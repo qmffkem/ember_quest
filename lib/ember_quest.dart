48972586313a3e92ce9e865b6e77eabeefd22f8e
qmffkem <wodndckato@gmail.com>
2024-01-13 21:04:38 -0500

    adding ember into the map

---
+++ lib/ember_quest.dart
@@ -0,0 +1,28 @@
+import "package:flame/game.dart";
+import "package:flame/components.dart";
+
+import "actors/ember.dart";
+
+class EmberQuestGame extends FlameGame {
+  late EmberPlayer _ember;
+
+  @override
+  Future<void> onLoad() async {
+    await images.loadAll([
+      "block.png",
+      "ember.png",
+      "ground.png",
+      "heart_half.png",
+      "heart.png",
+      "star.png",
+      "water_enemy.png",
+    ]);
+
+    camera.viewfinder.anchor = Anchor.topLeft;
+
+    _ember = EmberPlayer(
+      position: Vector2(128, canvasSize.y - 70),
+    );
+    world.add(_ember);
+  }
+}
