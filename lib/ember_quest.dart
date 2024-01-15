609e6fc657261f60876ee18116333ec9635f5626
qmffkem <wodndckato@gmail.com>
2024-01-14 19:36:15 -0500

    add segment manager into map and set background

--- lib/ember_quest.dart
+++ lib/ember_quest.dart
@@ -1,10 +1,17 @@
+import "package:ember_quest/actors/water_enemy.dart";
+import "package:ember_quest/managers/segment_manager.dart";
+import "package:ember_quest/objects/ground_block.dart";
+import "package:ember_quest/objects/platform_block.dart";
+import "package:ember_quest/objects/star.dart";
 import "package:flame/game.dart";
 import "package:flame/components.dart";
+import "package:flutter/widgets.dart";
 
 import "actors/ember.dart";
 
 class EmberQuestGame extends FlameGame {
   late EmberPlayer _ember;
+  double objectSpeed = 0.0;
 
   @override
   Future<void> onLoad() async {
@@ -19,10 +26,43 @@ class EmberQuestGame extends FlameGame {
     ]);
 
     camera.viewfinder.anchor = Anchor.topLeft;
+    initializeGame();
+  }
+
+  void loadGameSegments(int segmentIndex, double xPositionOffset) {
+    for (final block in segments[segmentIndex]) {
+      switch (block.blockType) {
+        case GroundBlock:
+          break;
+        case PlatformBlock:
+          add(PlatformBlock(
+            gridPosition: block.gridPosition,
+            xOffset: xPositionOffset,
+          ));
+          break;
+        case Star:
+          break;
+        case WaterEnemy:
+          break;
+      }
+    }
+  }
+
+  @override
+  Color backgroundColor() => const Color.fromARGB(255, 173, 223, 247);
+
+  void initializeGame() {
+    final segmentsToLoad = (size.x / 640).ceil();
+    segmentsToLoad.clamp(0, segments.length);
+
+    for (var i = 0; i <= segmentsToLoad; i++) {
+      loadGameSegments(i, (640 * i).toDouble());
+    }
 
     _ember = EmberPlayer(
       position: Vector2(128, canvasSize.y - 70),
     );
+
     world.add(_ember);
   }
 }
